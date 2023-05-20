1233d42751eb6a78e04d8a1076190e4e0d62d1d1
pedroabp <pedroabp5@gmail.com>
2023-05-19 20:48:40 -0500

    Allow user to listen the uploaded audio file

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 
 app = Flask(__name__)
 
@@ -6,8 +6,16 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.post("/")
-def index_post():
+@app.post("/upload_file")
+def upload_file():
     f = request.files['file']
     f.save('upload.wav')
-    return 'Saved'
+    return render_template('upload_file.html')
+
+@app.get("/download_file")
+def download_file():
+    file_path = 'upload.wav'
+    mime_type = 'audio/wav'
+
+    return send_file(file_path, mimetype=mime_type)
+
